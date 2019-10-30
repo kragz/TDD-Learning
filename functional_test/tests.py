4a37c147183c2183c12806a5f89a7558c1b50672
root <root@localhost.localdomain>
2019-10-30 10:09:33 -0400

    make functional_tests an app, use LiveServerTestCase

--- functional_test/tests.py
+++ functional_test/tests.py
@@ -1,10 +1,10 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import unittest
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -21,7 +21,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         #John has heard about a cool new online to-do app. He goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # He notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -52,7 +52,7 @@ class NewVisitorTest(unittest.TestCase):
 
 
         self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         '''table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
@@ -63,14 +63,4 @@ class NewVisitorTest(unittest.TestCase):
         self.fail('Finish the test!')
 
 
-if __name__ == "__main__":
-    unittest.main(warnings='ignore')
-
-
-'''browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
-
-assert 'To-Do' in browser.title, "Browser title was " + browser.title
-
-browser.quit()'''
 
